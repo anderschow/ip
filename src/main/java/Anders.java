7023418275d9462ea2bd3f6c8a3efc74a805c004
Anders Chow <anderschow28@gmail.com>
2026-08-19 00:12:40 +0800

    Greet User and Exit

--- src/main/java/Anders.java
+++ src/main/java/Anders.java
@@ -1,10 +1,21 @@
+/**
+ * The entry point for the Anders chatbot.
+ */
 public class Anders {
     public static void main(String[] args) {
         String banner = "    _                 _                \n"
                 + "   / \\   _ __   __| | ___ _ __ ___\n"
                 + "  / _ \\ | '_ \\ / _` |/ _ \\ '__/ __|\n"
                 + " / ___ \\| | | | (_| |  __/ |  \\__ \\\n"
-                + "/_/   \\_\\_| |_|\\__,_|\\___|_|  |___/\n";
+                + "/_/   \\_\\_| |_|\\__,_|\\___|_|  |___/";
+        String separator = "____________________________________________________________";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Anders.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
