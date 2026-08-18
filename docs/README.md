0e5d220558e50298d53292e7f26d421c544bde11
Anders Chow <anderschow28@gmail.com>
2026-08-19 00:04:57 +0800

    Renamed Chatbot to Anders

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Anders User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Anders.java
@@ -0,0 +1,10 @@
+public class Anders {
+    public static void main(String[] args) {
+        String banner = "    _                 _                \n"
+                + "   / \\   _ __   __| | ___ _ __ ___\n"
+                + "  / _ \\ | '_ \\ / _` |/ _ \\ '__/ __|\n"
+                + " / ___ \\| | | | (_| |  __/ |  \\__ \\\n"
+                + "/_/   \\_\\_| |_|\\__,_|\\___|_|  |___/\n";
+        System.out.println(banner);
+    }
+}
