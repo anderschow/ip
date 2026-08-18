fee7f3953fed1d463f465e80435fcba8480535da
Anders Chow <anderschow28@gmail.com>
2026-08-19 01:29:24 +0800

    Added Mark Command

--- src/main/java/Anders.java
+++ src/main/java/Anders.java
@@ -21,6 +21,7 @@ public class Anders {
         System.out.println(separator);
 
         String[] tasks = new String[100];
+        boolean[] completed = new boolean[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -34,8 +35,24 @@ public class Anders {
             }
 
             if (command.equals("list")) {
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                    String status = completed[i] ? "X" : " ";
+                    System.out.println("     " + (i + 1) + ".[" + status + "] " + tasks[i]);
+                }
+            } else if (command.startsWith("mark ")) {
+                String taskNumber = command.substring("mark ".length()).trim();
+                try {
+                    int taskIndex = Integer.parseInt(taskNumber) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        completed[taskIndex] = true;
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       [X] " + tasks[taskIndex]);
+                    } else {
+                        System.out.println("     Task number must be between 1 and " + taskCount + ".");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("     Please provide a valid task number.");
                 }
             } else if (taskCount < tasks.length) {
                 tasks[taskCount] = command;
