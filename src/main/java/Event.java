de13a6f246c3ebd5818aa7952c624d085e6d6338
Anders Chow <anderschow28@gmail.com>
2026-08-19 20:44:21 +0800

    Added Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,26 @@
+/**
+ * Represents a task scheduled between a start time and an end time.
+ */
+public class Event extends Task {
+    private final String from;
+    private final String to;
+
+    /**
+     * Creates a new unfinished event task.
+     *
+     * @param description the task description
+     * @param from the event start time, kept as entered by the user
+     * @param to the event end time, kept as entered by the user
+     */
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    /** Returns the display text for this event task. */
+    @Override
+    public String toString() {
+        return "[E] " + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
